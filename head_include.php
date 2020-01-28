be88c1646c7cc8f9c6a1ac99f0d65906e533790f
Matt Culin <43552534+MatthewCulin@users.noreply.github.com>
2020-01-28 10:58:37 -0500

    Template/Manage

--- head_include.php
+++ head_include.php
@@ -1,7 +1,6 @@
-<head>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link type="text/css" rel="stylesheet" href="css/reset.css" />
   <link type="text/css" rel="stylesheet" href="css/default.css" />
-  <title> PROJECT INDEX PAGE </title>
-</head>
+
