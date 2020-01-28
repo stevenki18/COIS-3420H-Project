86c7f916a9702cd421591d5eda4b7a77176a70eb
Matt Culin <43552534+MatthewCulin@users.noreply.github.com>
2020-01-28 10:43:18 -0500

    Copy files from Steven

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<!-- ******************************** Filename: index.html, uses default.css
+ *
+ * Usage: Bucket List Splash PAGE
+ *        This is the first page when coming to the website to create or modify
+ *        a bucket list. Work in progress
+ *
+ *
+ * Note: reset.css is also being used by the q3.html files in order to wipe settings
+ *
+ * Created by: Steven Ki for COIS-3420H Assignment 1, Question 3
+ ******************************************************************************
+ -->
+<html lang="en">
+
+<?php include 'head_include.php' ?>
+
+<body>
+  <!-- SPLASH PAGE -->
+  <!-- <div class = "container"> -->
+  <?php include 'header.php' ?>
+
+  <main>
+    <!-- OUR GENERIC LIST -->
+    <!-- DEFAULT LIST -->
+  </main>
+
+
+  <?php include 'footer.php' ?>
+  
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,19 @@
+<nav>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li class="dropdown">Lists
+      <ul class="dropdown-content">
+        <li><a href="#">Things to Do Before I'm 40</a></li>
+        <li><a href="#">Our Sample List</a></li>
+        <li><a href="#">Add List</a></li>
+      </ul>
+    </li>
+    <li>
+      <form>
+        <input type="text" name="submit-search" placeholder="Search Lists">
+        <input type="submit" name="submit-search" label="Search" />
+      </form>
+    </li>
+    <li><a href="#">Login</a></li>
+  </ul>
+</nav>
