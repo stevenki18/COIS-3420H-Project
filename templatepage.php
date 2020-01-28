6e934def976b4c0fb605fea838e44e2b07022b19
stevenki18 <stevenki@trentu.ca>
2020-01-28 11:01:43 -0500

    Add Template

---
+++ templatepage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include 'head_include.php' ?>
+        <title></title>
+    </head>
+    <body>
+        <!-- HEADER -->
+        <header>
+            <?php include 'nav.php' ?>
+        </header>
+
+        <main>
+
+        </main>
+
+        <!-- FOOTER -->
+        <?php include 'footer.php' ?>
+
+    </body>
+</html>
