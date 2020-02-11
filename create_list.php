3819b7c7a1f9e7ebf35397dec97e7c4a054d303a
stevenki18 <stevenki@trentu.ca>
2020-02-10 23:22:06 -0500

    Got 2 modals working off NAV
    Login/Create_list modals now working

--- create_list.php
+++ create_list.php
@@ -24,7 +24,7 @@
                         <input type="text" id="add-list" name="add-list" placeholder="List Name"/>
                         <button type="button" name="add-list" id="add-list"><i class="fa fa-unlock"></i></button>
                     </div>
-                        
+
                     <!-- SUBMIT -->
                     <input type="submit" value="Create List"/>
 
@@ -38,31 +38,3 @@
         </div>
 
 </div>
-
-<script>
-    // Get the modal
-    var modal = document.getElementById("create-modal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("create-btn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-</script>
--- nav.php
+++ nav.php
@@ -12,7 +12,7 @@
       <ul class="dropdown-content">
         <li><a href="manage_list.php">Things to Do Before I'm 40</a></li>
         <li><a href="manage_list.php">Our Sample List</a></li>
-        <li><a href = "#opencreate" id = "create-btn">Add List</a></li>
+        <li><a href = "#create_modal" id="create-btn" class="modBtn">Add List</a></li>
       </ul>
     </li>
     <li>
@@ -23,15 +23,52 @@
         </form>
       </div>
     </li>
-    <li><a href = "#openlogin" id = "login-btn">Login</a><!--<a href="register.php">Login</a>--></li>
+    <li><a href = "#login-modal" id = "login-btn" class="modBtn">Login</a><!--<a href="register.php">Login</a>--></li>
   </ul>
 
-  <div id="opencreate">
-    <?php include "create_list.php" ?>
-  </div>
+  <?php include "login.php" ?>
 
-  <div id="openlogin">
-    <?php include "login.php" ?>
-  </div>
+  <?php include "create_list.php" ?>
+
+  <script>
+      // Get the modal
+      var modal = document.getElementsByClassName('modal');
+
+      // Get the button that opens the modal
+      var btn = document.getElementsByClassName("modBtn");
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close");
+
+      // When the user clicks the button, open the modal
+      btn[0].onclick = function() {
+          modal[1].style.display = "block";
+      }
+
+      // When the user clicks the button, open the modal
+      btn[1].onclick = function() {
+          modal[0].style.display = "block";
+      }
+
+      // When the user clicks on <span> (x), close the modal
+      span[0].onclick = function() {
+          modal[0].style.display = "none";
+      }
+
+      // When the user clicks on <span> (x), close the modal
+      span[1].onclick = function() {
+          modal[1].style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal[0]) {
+              modal[0].style.display = "none";
+          }
+          if (event.target == modal[1]) {
+              modal[1].style.display = "none";
+          }
+      }
+  </script>
 
 </nav>
