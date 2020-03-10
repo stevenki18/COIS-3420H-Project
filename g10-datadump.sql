613c487ef369fb935c2d5e5b134822fc2c7d3d1c
Matt Culin <43552534+MatthewCulin@users.noreply.github.com>
2020-03-10 12:53:42 -0400

    database

--- g10-datadump.sql
+++ g10-datadump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 03, 2020 at 09:41 PM
+-- Generation Time: Mar 10, 2020 at 04:51 PM
 -- Server version: 5.7.29
 -- PHP Version: 7.1.33
 
@@ -19,8 +19,39 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `stevenki`
+-- Database: `matthewculin`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `g10_listitems`
+--
+
+CREATE TABLE `g10_listitems` (
+  `id` int(11) NOT NULL,
+  `fk_listid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(280) DEFAULT NULL,
+  `picpath` varchar(255) DEFAULT NULL,
+  `completion` date DEFAULT NULL,
+  `private` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `g10_lists`
+--
+
+CREATE TABLE `g10_lists` (
+  `id` int(11) NOT NULL,
+  `fk_userid` int(11) NOT NULL,
+  `listname` varchar(50) NOT NULL,
+  `start` date NOT NULL,
+  `end` date DEFAULT NULL,
+  `private` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -35,20 +66,33 @@ CREATE TABLE `g10_users` (
   `first` varchar(64) NOT NULL,
   `last` varchar(64) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `dob` date NOT NULL,
-  `tac` tinyint(1) NOT NULL
+  `dob` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `g10_users`
 --
 
-INSERT INTO `g10_users` (`id`, `username`, `pass`, `first`, `last`, `email`, `dob`, `tac`) VALUES
-(1, 'master', '$2y$10$N/.jPwKf1YTiVGTCK06jLeUCuLiZjNzDQfp543IbG33hUkI9yqUUW', 'Halo', 'Master', 'hmaster@email.com', '1900-01-05', 1);
+INSERT INTO `g10_users` (`id`, `username`, `pass`, `first`, `last`, `email`, `dob`) VALUES
+(1, 'master', '$2y$10$N/.jPwKf1YTiVGTCK06jLeUCuLiZjNzDQfp543IbG33hUkI9yqUUW', 'Halo', 'Master', 'hmaster@email.com', '1900-01-05');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `g10_listitems`
+--
+ALTER TABLE `g10_listitems`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `g10_listid_2_fklistid` (`fk_listid`);
+
+--
+-- Indexes for table `g10_lists`
+--
+ALTER TABLE `g10_lists`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `g10_userid_2_fkuserid` (`fk_userid`);
 
 --
 -- Indexes for table `g10_users`
@@ -61,10 +105,38 @@ ALTER TABLE `g10_users`
 --
 
 --
+-- AUTO_INCREMENT for table `g10_listitems`
+--
+ALTER TABLE `g10_listitems`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `g10_lists`
+--
+ALTER TABLE `g10_lists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `g10_users`
 --
 ALTER TABLE `g10_users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `g10_listitems`
+--
+ALTER TABLE `g10_listitems`
+  ADD CONSTRAINT `g10_listid_2_fklistid` FOREIGN KEY (`fk_listid`) REFERENCES `g10_lists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `g10_lists`
+--
+ALTER TABLE `g10_lists`
+  ADD CONSTRAINT `g10_userid_2_fkuserid` FOREIGN KEY (`fk_userid`) REFERENCES `g10_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
