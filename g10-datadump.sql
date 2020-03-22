dda8065a72107f847781e8f6a8870a6d8ee7b0ee
Matt Culin <43552534+MatthewCulin@users.noreply.github.com>
2020-03-22 15:34:36 -0400

    Data Dump

--- g10-datadump.sql
+++ g10-datadump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 10, 2020 at 04:51 PM
+-- Generation Time: Mar 22, 2020 at 07:33 PM
 -- Server version: 5.7.29
 -- PHP Version: 7.1.33
 
@@ -28,15 +28,34 @@ SET time_zone = "+00:00";
 -- Table structure for table `g10_listitems`
 --
 
-CREATE TABLE `g10_listitems` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `g10_listitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `fk_listid` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `description` varchar(280) DEFAULT NULL,
   `picpath` varchar(255) DEFAULT NULL,
   `completion` date DEFAULT NULL,
-  `private` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `private` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `g10_listid_2_fklistid` (`fk_listid`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `g10_listitems`
+--
+
+INSERT INTO `g10_listitems` (`id`, `fk_listid`, `name`, `description`, `picpath`, `completion`, `private`) VALUES
+(1, 1, 'Don\'t die!', 'Try my best to not die', NULL, NULL, 1),
+(2, 1, 'Ride a camel', 'Ride a camel in the desert', NULL, NULL, 0),
+(3, 2, 'Don\'t die', 'If I live this long, try not to die again', NULL, NULL, 1),
+(4, 2, 'Go skydiving', 'Go skydiving from 13,000 ft', NULL, NULL, 0),
+(5, 3, 'Add Items', 'Get add items working', NULL, NULL, 0),
+(6, 3, 'View Items', 'Get view items working', NULL, NULL, 0),
+(7, 3, 'Edit Items', 'Get edit items working', NULL, NULL, 0),
+(8, 3, 'Delete Items', 'Get delete items working', NULL, NULL, 1),
+(9, 3, 'Add List', 'Get add list working', NULL, NULL, 0),
+(10, 3, 'Manage List', 'Get my list page to link to manage list with proper list id', NULL, NULL, 1),
+(11, 3, 'View Lists Page', 'Get view lists page to pull all public lists', NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -44,14 +63,25 @@ CREATE TABLE `g10_listitems` (
 -- Table structure for table `g10_lists`
 --
 
-CREATE TABLE `g10_lists` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `g10_lists` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `fk_userid` int(11) NOT NULL,
   `listname` varchar(50) NOT NULL,
   `start` date NOT NULL,
   `end` date DEFAULT NULL,
-  `private` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `private` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `g10_userid_2_fkuserid` (`fk_userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `g10_lists`
+--
+
+INSERT INTO `g10_lists` (`id`, `fk_userid`, `listname`, `start`, `end`, `private`) VALUES
+(1, 1, 'Complete Me By 50', '2020-03-20', NULL, 1),
+(2, 1, 'Complete Me By 90', '2020-03-20', '2020-03-22', 0),
+(3, 1, 'Things To Do', '2020-03-21', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -59,68 +89,24 @@ CREATE TABLE `g10_lists` (
 -- Table structure for table `g10_users`
 --
 
-CREATE TABLE `g10_users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `g10_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(32) NOT NULL,
   `pass` varchar(128) NOT NULL,
   `first` varchar(64) NOT NULL,
   `last` varchar(64) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `dob` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `dob` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `g10_users`
 --
 
 INSERT INTO `g10_users` (`id`, `username`, `pass`, `first`, `last`, `email`, `dob`) VALUES
-(1, 'master', '$2y$10$N/.jPwKf1YTiVGTCK06jLeUCuLiZjNzDQfp543IbG33hUkI9yqUUW', 'Halo', 'Master', 'hmaster@email.com', '1900-01-05');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `g10_listitems`
---
-ALTER TABLE `g10_listitems`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `g10_listid_2_fklistid` (`fk_listid`);
-
---
--- Indexes for table `g10_lists`
---
-ALTER TABLE `g10_lists`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `g10_userid_2_fkuserid` (`fk_userid`);
-
---
--- Indexes for table `g10_users`
---
-ALTER TABLE `g10_users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `g10_listitems`
---
-ALTER TABLE `g10_listitems`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `g10_lists`
---
-ALTER TABLE `g10_lists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `g10_users`
---
-ALTER TABLE `g10_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+(1, 'master', '$2y$10$N/.jPwKf1YTiVGTCK06jLeUCuLiZjNzDQfp543IbG33hUkI9yqUUW', 'Halo', 'Master', 'hmaster@email.com', '1900-01-05'),
+(2, 'matthewculin', '$2y$10$oLdbgAhG6AEQ4Zpk1XNHQebQCA6lrLvOhWHjz099eooLNUo8O.gru', 'Matthew', 'Culin', 'matthewculin@trentu.ca', '1997-10-15');
 
 --
 -- Constraints for dumped tables
